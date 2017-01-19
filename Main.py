1f1ad10cf7313153e8a1c53292c91fe25c1ea7a6
lisssek <lisssek@gmail.com>
2017-01-19 12:33:21 +0000

    Enhancements to Main.py
    These changes reflect the changes in the MainFunctions4.py which should replace the mainFunction.py.

--- Main.py
+++ Main.py
@@ -25,52 +25,66 @@ def input_dropdown():
         if term != '':
             source = request.form['source']
             match = request.form['match']
-            starting_classification = request.form['start']
-            destination_classification = request.form['destination']
-            results_in_list, DownloadUrl = clean_variables(source, match, term)
-            translation = translations(
-                results_in_list,
-                source,
-                starting_classification,
-                destination_classification,
-            )
-            number_of_results = len(results_in_list)
-            number_of_translations = len(translation)
-            links = []
-            for i in results_in_list:
-                a = i.split(':')
-                uri = a[1] + ':' + a[2]
-                links.append(uri)
+            if match!='' and source =='er':
+                flash('Please select project you would like to search.')
+                return render_template("translator2.html")
+            else:
+                starting_classification = request.form['start']
+                destination_classification = request.form['destination']
+                results_in_list, DownloadUrl = clean_variables(source, match, term)
+                translation = translations(
+                    results_in_list,
+                    source,
+                    starting_classification,
+                    destination_classification,
+                )
+                number_of_results = len(results_in_list)
+                number_of_translations = len(translation)
+                links = []
+                links_to_translations=[]
+					for i in results_in_list:
+						a=i.split(':')
+						uri=a[1]+':'+a[2]
+						links.append(uri)
+					for ii in translation:
+						b=ii.split(':')
+						uri2=b[1]+':'+b[2]
+						links_to_translations.append(uri2)
         if code != '':
             source = request.form['source']
             match = request.form['match']
-            starting_classification = request.form['start']
-            destination_classification = request.form['destination']
-            if source == 'er':
-                flash('You forgot to choose your starting data standard!')
-                return render_template("translator2.html")
-            results_in_list, DownloadUrl = clean_variables2(
-                source, match, code)
-            translation = translations2(
-                results_in_list,
-                source,
-                starting_classification,
-                destination_classification)
-            number_of_results = len(results_in_list)
-            number_of_translations = len(translation)
-            links = []
-            for i in results_in_list:
-                a = i.split(':')
-                if (a[0] == 'Original code' or a[0] == 'Translated code'):
-                    links.append('')
-                else:
-                    uri = a[1] + ':' + a[2]
-                    links.append(uri)
+            if match!=''and source =='er':
+                flash('Please select project you would like to search.')
+                return render_template("translator2.html")	
+            else:					
+                starting_classification=request.form['start']
+                destination_classification=request.form['destination']
+                results_in_list, DownloadUrl =clean_variables2(source, match, code)
+                translation=translations2(results_in_list, source, starting_classification, destination_classification)
+                number_of_results=len(results_in_list)	
+                number_of_translations=len(translation)	
+                links=[]
+                links_to_translations=[]
+                for i in results_in_list:
+                    a=i.split(':')
+                    if (a[0]=='Search code' or a[0]=='Translated code'):
+                        links.append('')
+                    else:	
+                        uri=a[1]+':'+a[2]
+                        links.append(uri)
+                for ii in translation:
+                    b=ii.split(':')
+                    print b
+                    if (a[0]=='Search code' or a[0]=='Translated code'):
+                        links_to_translations.append('')
+                    else:
+                        uri2=b[1]+':'+b[2]
+                        links_to_translations.append(uri2)
 
     return jsonify(
         no=number_of_results,
         no2=number_of_translations,
-        trans=translation,
+        trans=zip(links_to_translations,translation),
         DownloadXML=DownloadUrl,
         Links_and_Matches=zip(
             links,
